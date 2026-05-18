4033e8a2be5f0f015b332bb1c8404a1cc39ce43e
Maria Malik <106521730@student.swin.edu.au>
2026-05-19 04:23:38 +1000

    finished draft

---
+++ search_form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Managing EOIs">
+    <meta name = "keywords" content="SQL, Database">
+    <meta name= "author" content="Maria Malik">
+</head>
+<body>
+
+<form method="get" action="search_results.php">
+<input type="text" name="model" id="model" >
+<input type="submit" name="search" value="Search models">
+</form>
+<br>
+
+</body>
+</html>
